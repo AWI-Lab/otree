bce32df541894a94e4245287f218103d4807c1ad
oTree-org <chris@otree.org>
2017-01-18 00:57:00 +0900

    suppress tracebacks when waiting for DB to start

--- pg_ping.py
+++ pg_ping.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 import psycopg2
+import sys
 
 from django.db import connections
 
-connection = psycopg2.connect(
-    database=connections['default'].settings_dict['NAME'],
-    user=connections['default'].settings_dict['USER'],
-    password=connections['default'].settings_dict['PASSWORD'],
-    host=connections['default'].settings_dict['HOST'],
-    port=connections['default'].settings_dict['PORT']
-)
+try:
+	connection = psycopg2.connect(
+	    database=connections['default'].settings_dict['NAME'],
+	    user=connections['default'].settings_dict['USER'],
+	    password=connections['default'].settings_dict['PASSWORD'],
+	    host=connections['default'].settings_dict['HOST'],
+	    port=connections['default'].settings_dict['PORT']
+	)
+except psycopg2.OperationalError as e:
+	sys.exit("The database is not ready.")
