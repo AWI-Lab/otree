4aa2ac0441b5c3eebec81a1dfe5c2440f2b06fa2
Christian König <koenig.kersting@gmail.com>
2017-03-20 16:25:55 +0100

    updated outcomebias

--- outcomebias/models.py
+++ outcomebias/models.py
@@ -25,8 +25,10 @@ class Constants(BaseConstants):
 	principal_fix_pay = 100
 	reward_pot = 100
 
-	long_timeout = 120
-	decision_timeout = 60
+	long_timeout = 180
+	decision_timeout = 180
+
+	LikertScale = [1, 2, 3, 4, 5, 6, 7]
 
 
 class Subsession(BaseSubsession):
@@ -35,12 +37,15 @@ class Subsession(BaseSubsession):
 
 class Group(BaseGroup):
 	treatment = models.CharField()
-	winning_color = models.CharField(choices=['yellow', 'orange'])
+	winning_color = models.CharField(choices=['yellow', 'green'], widget=widgets.RadioSelectHorizontal)
 	lottery_outcome = models.CharField()
 	lottery_pay = models.PositiveIntegerField(min=0, max=Constants.lottery_high_payoff)
 	reward_good = models.PositiveIntegerField(min=0, max=Constants.reward_pot)
 	reward_bad = models.PositiveIntegerField(min=0, max=Constants.reward_pot)
 	reward = models.PositiveIntegerField(min=0, max=Constants.reward_pot)
+
+	a_payoff = models.PositiveIntegerField()
+	b_payoff = models.PositiveIntegerField()
 
 	intact = models.BooleanField(initial=True)
 
@@ -48,7 +53,7 @@ class Group(BaseGroup):
 		self.treatment = 'agent' if self.id_in_subsession % 2 == 0 else 'computer'
 		self.lottery_outcome = random.choice(['good', 'bad'])
 		if self.treatment == 'computer':
-			self.winning_color = random.choice(['yellow', 'orange'])
+			self.winning_color = random.choice(['yellow', 'green'])
 
 	def set_payoffs(self):
 		if self.intact:
@@ -57,14 +62,23 @@ class Group(BaseGroup):
 			for player in self.get_players():
 				if player.role() == "agent":
 					player.payoff = Constants.agent_fix_pay + self.reward
+					self.a_payoff = player.payoff
 				else:
 					player.payoff = Constants.principal_fix_pay + (Constants.reward_pot - self.reward) + self.lottery_pay
+					self.b_payoff = player.payoff
 		else:
 			for player in self.get_players():
 				if player.role() == "agent":
 					player.payoff = Constants.agent_fix_pay
+					self.a_payoff = player.payoff
 				else:
 					player.payoff = Constants.principal_fix_pay	
+					self.b_payoff = player.payoff
+
+		for player in self.get_players():
+			player.GXPProfit = player.payoff
+
+
 
 class Player(BasePlayer):
 	
@@ -74,4 +88,39 @@ class Player(BasePlayer):
 		else:
 			return 'principal'
 
-		timed_out = models.BooleanField(initial=False)
+
+	timed_out = models.BooleanField(initial=False)
+
+	outcome_satisfaction = models.PositiveIntegerField(choices=Constants.LikertScale, widget=widgets.RadioSelectHorizontal)
+	decision_satisfaction = models.PositiveIntegerField(choices=Constants.LikertScale, widget=widgets.RadioSelectHorizontal)
+
+	expected_transfer_good = models.PositiveIntegerField(min=0, max=100)
+	expected_transfer_bad = models.PositiveIntegerField(min=0, max=100)
+
+	age = models.PositiveIntegerField(verbose_name="How old are you?")
+	gender = models.CharField(
+		choices=['female', 'male', 'other', 'prefer not to tell'],
+		verbose_name="What is your gender?",
+		widget=widgets.RadioSelectHorizontal)
+
+	education = models.PositiveIntegerField(
+		choices=[
+			[1, 'some High School'], 
+			[2, 'High School graduate'],
+			[3, 'some College, no degree'],
+			[4, "Associate's degree"],
+			[5, "Bachelor's degree"],
+			[6, "Master's degree"],
+			[7, "Doctorate degree"]
+		 ],
+		 verbose_name="What is the highest level of education you have attained?",
+		 widget=widgets.RadioSelect)
+	
+	studies = models.CharField(
+		blank=True, 
+		verbose_name="If you have at least some college education, what is/was your field of studies?")
+
+	occupation = models.CharField(verbose_name="What is your main occupation?")
+
+	GXPProfit = models.CurrencyField()
+
