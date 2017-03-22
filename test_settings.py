29cf839c74f41759fbe80c1f8c9057dd040feffa
Tukkin Moriarty <mrtukkin@gmail.com>
2017-03-22 20:00:34 +0100

    Travis-CI Test 3

---
+++ test_settings.py
@@ -0,0 +1,74 @@
+import os
+from os import environ
+
+import dj_database_url
+
+import otree.settings
+
+from session_configs import SESSION_CONFIGS
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+DEBUG = True
+
+# don't share this with anybody.
+SECRET_KEY = 'secret_key'
+
+# To use a database other than sqlite,
+# set the DATABASE_URL environment variable.
+# Examples:
+# postgres://USER:PASSWORD@HOST:PORT/NAME
+# mysql://USER:PASSWORD@HOST:PORT/NAME
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    )
+}
+
+# AUTH_LEVEL:
+# this setting controls which parts of your site are freely accessible,
+# and which are password protected:
+# - If it's not set (the default), then the whole site is freely accessible.
+# - If you are launching a study and want visitors to only be able to
+#   play your app if you provided them with a start link, set it to STUDY.
+# - If you would like to put your site online in public demo mode where
+#   anybody can play a demo version of your game, but not access the rest
+#   of the admin interface, set it to DEMO.
+
+# for flexibility, you can set it in the environment variable OTREE_AUTH_LEVEL
+AUTH_LEVEL = environ.get('OTREE_AUTH_LEVEL')
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+# e.g. EUR, CAD, GBP, CHF, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'EUR'
+USE_POINTS = True
+
+
+# e.g. en, de, fr, it, ja, zh-hans
+# see: https://docs.djangoproject.com/en/1.9/topics/i18n/#term-language-code
+LANGUAGE_CODE = 'de'
+
+# if an app is included in SESSION_CONFIGS, you don't need to list it here
+INSTALLED_APPS = ['otree']
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = {
+    'real_world_currency_per_point': 0.00,
+    'participation_fee': 0.00,
+    'doc': "",
+    'mturk_hit_settings': mturk_hit_settings,
+}
+
+# anything you put after the below line will override
+# oTree's default settings. Use with caution.
+otree.settings.augment_settings(globals())
